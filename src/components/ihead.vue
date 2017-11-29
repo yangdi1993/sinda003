1826065df51019a2dcf5480c50231fb9f3a8fe5d
yangdi <736837490@qq.com>
2017-11-29 16:41:11 +0800

    '整体框架'

---
+++ src/components/ihead.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h3>这是系统内部头部</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+h3{
+  color:red;
+  font-family: '宋体';
+  width: 100%;
+  height: 150px;
+  background: pink;
+}
+</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ src/components/inner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="inner">
+    <p>这是系统内部</p>
+    <ihead></ihead>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import ihead from './ihead'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{ihead}
+}
+</script>
+
+<style scoped lang="less">
+*{margin: 0;padding: 0;border: 0;}
+.inner{
+  color:red;
+  width: 100%;
+  background: #1da;
+  font-family: '宋体';
+  padding-bottom: 50px;
+}
+</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
