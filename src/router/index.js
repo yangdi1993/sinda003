a2c9f3fa893046880b09db7addb76655f1ea0f9c
liuchengdong <634730693@qq.com>
2017-12-01 14:21:17 +0800

    会员新增

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ import inner from '@/components/inner'
 import login from '@/views/login'
 import register from '@/views/register'
 import homepage from '@/homepage/homepage'
+// 会员中心-我的订单
+import myOrder from '@/memberCenter/myOrder'
+// 会员中心-用户评价-未评价
+import userUnEvalu from '@/memberCenter/userUnEvalu'
+// 会员中心-用户评价-已评价
+import userHavaEvalu from '@/memberCenter/userHavaEvalu'
 
 Vue.use(Router)
 
@@ -28,6 +34,24 @@ export default new Router({
           name: 'homepage',
           component: homepage
         },
+        // 会员中心-我的订单
+        {
+          path:'/myOrder',
+          name:'myOrder',
+          component:myOrder,
+        },
+        // 会员中心-用户评价-未评价
+        {
+          path:'/userUnEvalu',
+          name:'userUnEvalu',
+          component:userUnEvalu,
+        },
+        // 会员中心-用户评价-已评价
+        {
+          path:'/userHavaEvalu',
+          name:'userHavaEvalu',
+          component:userHavaEvalu,
+        }
       ]
     },
     {
@@ -51,6 +75,7 @@ export default new Router({
       path:'/server',
       name:'server',
       component:server,
-    }
+    },
+    
   ]
 })
