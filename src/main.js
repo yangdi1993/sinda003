325e79bd29b414720f6d64906afa1cffa652f265
skynet3150 <99489927@qq.com>
2017-12-08 09:50:59 +0800

    gd

--- src/main.js
+++ src/main.js
@@ -4,31 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 5048eb60b85b4f6dedadd3334fabab9e7697c7f5
+
 import store from './store'
 import qs from 'qs'
 //轮播部分插件引入
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.use(VueAwesomeSwiper)
-<<<<<<< HEAD
-=======
+
 //输入验证插件
 import Validator from 'vue-validator'
 Vue.use(Validator)
->>>>>>> 5048eb60b85b4f6dedadd3334fabab9e7697c7f5
-=======
-import qs from 'qs'
-//=======
+
 //轮播部分插件引入
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.use(VueAwesomeSwiper)
 import store from './store'
->>>>>>> aa450e6d16421de0ea50db5655ea3074e4d3390b
+
 
 Vue.prototype.ajax=axios
 Vue.prototype.qs=qs
@@ -42,9 +35,6 @@ new Vue({
   template: '<App/>',
   components: { App },
  
-});
-Vue.validator('tel', function (val) {
-  return /^[0-9]{11}$/.test(val)
 });
 
 
