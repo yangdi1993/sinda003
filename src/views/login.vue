949cdcf6bc6378701edb1ea6890f9272d8fc9845
jufang <864270238@qq.com>
2017-12-13 09:17:25 +0800

    jufang

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <div class="r-outter">
     <!--小导航-->
@@ -195,11 +197,6 @@ export default {
         this.imgWrong = false;
         this.imgRight = false;
       }
-<<<<<<< HEAD
-      
-
-=======
->>>>>>> c7619130630bb893e1d8e40c769519c915ed68e8
       // 判断登录条件
       //判断手机号存在
       this.ajax.post('/xinda-api/sso/login', this.qs.stringify({ loginId: this.loginPhone, password: this.loginPw, imgCode: this.codeImage })).then(data => {
@@ -239,16 +236,12 @@ export default {
           var loginUser = {};
           loginUser.username = this.loginPhone;
           loginUser.password = this.loginPw;
-<<<<<<< HEAD
-          // sessionStorage.setItem(this.loginPhone,JSON.stringify(loginUser));
-=======
           sessionStorage.setItem(this.loginPhone,JSON.stringify(loginUser));
 
           sessionStorage.setItem('zancun',JSON.stringify(this.loginPhone))  //此处为登录状态信息，登陆后判断状态是否为登录,杨迪留的
           location.replace('/#/inner/homepage')
           this.setNum(2)  //购物车物品数量
 
->>>>>>> c7619130630bb893e1d8e40c769519c915ed68e8
           // console.log('loginUser'+loginUser)
           sessionStorage.setItem('zancun',JSON.stringify(this.loginPhone))  //此处为登录状态信息，登陆后判断状态是否为登录
           location.replace('/#/inner/homepage')
